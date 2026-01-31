1129a566a150e6ed822eb924c00490fc83deb976
PSBVISION <punitsinghbisht1529@gmail.com>
2026-01-31 21:09:24 +0530

    init: initialize frontend with nextJs and authentication and backend with strapi headless cms

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,65 @@
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { Cookie, Refrigerator } from "lucide-react";
+import UserDropdown from "./UserDropdown";
+
+const Header = () => {
+  const user = null; // Replace with actual user fetching logic
+  return (
+    <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
+      <nav className="container mx-auto px-4 h-16 flex justify-between items-center">
+        <Link
+          href={user ? "/dashboard" : "/"}
+          className="flex items-center gap-1.5 font-bold text-3xl text-stone-900"
+        >
+          <Image src="/logo.png" alt="Logo" width={60} height={60} />
+          ChefMate
+        </Link>
+        <div className="hidden md:flex items-center space-x-8 text-sm font-medium text-stone-600">
+          <Link
+            href="/recipes"
+            className="hover:text-orange-600 transition-colors flex gap-1.5 items-center"
+          >
+            <Cookie className="w-4 h-4" />
+            My Recipes
+          </Link>
+          <Link
+            href="/pantry"
+            className="hover:text-orange-600 transition-colors flex gap-1.5 items-center"
+          >
+            <Refrigerator className="w-4 h-4" />
+            My Pantry
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <UserDropdown/>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button
+                variant="outline"
+                className="text-stone-600 border-orange-300 hover:bg-orange-50 hover:border-orange-700"
+              >
+                {" "}
+                Sign In
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button variant="primary">Sign Up</Button>
+            </SignUpButton>
+          </SignedOut>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
